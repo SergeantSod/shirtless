225f8a2e876954ed10b366b16b377e6a2590c45d
Johannes Pirkl <johannespirkl@gmx.de>
2017-11-08 22:39:16 +0100

    Better examples.

--- src/main/scala/shirtless/Example.scala
+++ src/main/scala/shirtless/Example.scala
@@ -19,25 +19,29 @@ object Example {
     case class Banana(weight: Double)
     case class Monkey(name: String, age: Int)
     case class Human(name: String, age: Int)
+    case class GenericCaseClass[T](name: T, age: Int)
 
     //Explicit opt-in is required to define implicits (excuse the pun)
     implicit val c1 = Conversion fromCompanion Human
     implicit val c2 = Conversion fromCompanion Monkey
     implicit val c3 = Conversion fromCompanion Banana
-
+    implicit def c4[T] = Conversion.fromCompanion(GenericCaseClass[T](_,_))
 
 
     val george: Human = Human("George", 12)
 
-    println{
-      val monkey: Monkey = convert(george).to[Monkey]
-      monkey == Monkey("George", 12)
-    }
+    val asHList: String ~: Int ~: Empty = convert(george).toHList
+    println(asHList)
+    println(asHList == "George" ~: 12 ~: Empty)
 
-    println{
-      val hList: String ~: Int ~: Empty = convert(george).toHList
-      hList == "George" ~: 12 ~: Empty
-    }
+    val asMonkey: Monkey = convert(george).to[Monkey]
+    println(asMonkey)
+    println(asMonkey == Monkey("George", 12))
+
+    val asGeneric: GenericCaseClass[String] = convert(george).to[GenericCaseClass[String]]
+    println(asGeneric)
+    println(asGeneric == GenericCaseClass("George", 12))
+
 
   }
 }
